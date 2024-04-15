08f62db0e1b3ddbfda6270ba4aa0f46066f82f71
Oséias <149209851+JucaClan@users.noreply.github.com>
2024-04-15 17:33:44 -0300

    .

--- Atividade avaliativa - Upload de Arquivos/upload.php
+++ Atividade avaliativa - Upload de Arquivos/upload.php
@@ -4,24 +4,29 @@
 $resultado = [
     "Arquivo enviado com sucesso!",
     "Falha ao enviar arquivo!",
-
 ];
 
 $destino = 'imagens/' . $_FILES['arquivo']['name'];
  
 $arquivo_tmp = $_FILES['arquivo']['tmp_name'];
+
+//$extensoesValidas = array("jpeg", "jpg", "png","gif");
+
+$extensaoImagem = strtolower(pathinfo($destino,PATHINFO_EXTENSION));
  
-move_uploaded_file( $arquivo_tmp, $destino  );
 
-
+if (($extensaoImagem == 'jpg' ){
+move_uploaded_file($arquivo_tmp, $destino);
 
     echo 'Nome: ' , $_FILES[ 'arquivo' ][ 'name' ] ,'<br>' ;
     echo 'Tipo: ' , $_FILES[ 'arquivo' ][ 'type' ]  ,'<br>';
     echo 'tamanho: ' , $_FILES[ 'arquivo' ][ 'size' ]  ,'<br>';
+    echo 'Extensão: ' , $extensaoImagem ,'<br>';
+    echo '<br>';
+    echo '<img src="' . $destino . '" width="250px">';
 
-    header "<img
-    src="$destino"
-    alt="imagem recebida" />
+}  
+
 ?>
    
     
